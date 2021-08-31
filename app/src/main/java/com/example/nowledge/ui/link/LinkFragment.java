6919e479d42e1575a584ecee23481fdb73d45b66
Ericx <bhsfxty@163.com>
2021-08-31 15:15:47 +0800

    change labels of the navbar

--- app/src/main/java/com/example/nowledge/ui/link/LinkFragment.java
+++ app/src/main/java/com/example/nowledge/ui/link/LinkFragment.java
@@ -1,4 +1,4 @@
-package com.example.nowledge.ui.notifications;
+package com.example.nowledge.ui.link;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -12,24 +12,23 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
-import com.example.nowledge.R;
-import com.example.nowledge.databinding.FragmentNotificationsBinding;
+import com.example.nowledge.databinding.FragmentLinkBinding;
 
-public class NotificationsFragment extends Fragment {
+public class LinkFragment extends Fragment {
 
-    private NotificationsViewModel notificationsViewModel;
-    private FragmentNotificationsBinding binding;
+    private LinkViewModel linkViewModel;
+    private FragmentLinkBinding binding;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        notificationsViewModel =
-                new ViewModelProvider(this).get(NotificationsViewModel.class);
+        linkViewModel =
+                new ViewModelProvider(this).get(LinkViewModel.class);
 
-        binding = FragmentNotificationsBinding.inflate(inflater, container, false);
+        binding = FragmentLinkBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textNotifications;
-        notificationsViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+        final TextView textView = binding.textLink;
+        linkViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
                 textView.setText(s);

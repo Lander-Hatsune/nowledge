369616cd32696034d85de6cbe0a8e56df2b47c1a
Lander-Hatsune <wangwwx@yeah.net>
2021-09-02 09:40:59 +0800

    star/unstar

--- app/src/main/java/com/example/nowledge/EntityDetailActivity.java
+++ app/src/main/java/com/example/nowledge/EntityDetailActivity.java
@@ -1,11 +1,18 @@
 package com.example.nowledge;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.view.menu.ActionMenuItemView;
+import androidx.core.content.res.ResourcesCompat;
 
+import android.content.ClipData;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -14,6 +21,7 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.example.nowledge.data.Singleton;
 import com.example.nowledge.data.Uris;
+import com.example.nowledge.data.User;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -24,6 +32,9 @@ import java.util.List;
 
 public class EntityDetailActivity extends AppCompatActivity {
     private String id = Uris.id;
+    private Boolean starred = false;
+    private String name;
+    private String course;
 
     protected void updateId() {
         RequestQueue reqQue = Singleton.getInstance
@@ -68,13 +79,12 @@ public class EntityDetailActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        updateId();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_entity_detail);
 
         Bundle bundle = this.getIntent().getExtras();
-        String name = bundle.getString("name");
-        String course = bundle.getString("course");
+        name = bundle.getString("name");
+        course = bundle.getString("course");
 
         this.setTitle(name);
 
@@ -122,5 +132,102 @@ public class EntityDetailActivity extends AppCompatActivity {
             }
         });
         reqQue.add(req);
+
+        // star
+        url = Uris.getStarlist() + "?username=" + User.getUsername();
+        req = new JsonObjectRequest(Request.Method.GET, url, null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            String code = response.getString("id");
+                            if (!code.equals("0")) {
+                                return;
+                            }
+                            JSONArray starlist = response.getJSONArray("payload");
+                            for (int i = 0; i < starlist.length(); i++) {
+                                JSONObject star = starlist.getJSONObject(i);
+                                if (star.getString("course").equals(course) &&
+                                star.getString("name").equals(name)) {
+                                    starred = true;
+                                    ActionMenuItemView starmark = findViewById(R.id.actionStar);
+                                    MenuItem item = starmark.getItemData();
+                                    item.setIcon(R.drawable.star);
+                                }
+                            }
+                        } catch (JSONException e) {
+                            Log.e("Starlist error", e.toString());
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        });
+        reqQue.add(req);
+
     }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.detail_menu, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.actionStar:
+                String url;
+                if (starred) {
+                    url = Uris.getUnstar();
+                } else {
+                    url = Uris.getStar();
+                }
+                RequestQueue reqQue = Singleton.getInstance(getApplicationContext()).getRequestQueue();
+
+                JSONObject obj = new JSONObject();
+                try {
+                    obj.put("username", User.getUsername());
+                    obj.put("course", course);
+                    obj.put("name", name);
+                } catch (JSONException e) {
+                    Log.e("Star/Unstar obj err", e.toString());
+                }
+
+                JsonObjectRequest req = new JsonObjectRequest(Request.Method.POST, url, obj,
+                        new Response.Listener<JSONObject>() {
+                            @Override
+                            public void onResponse(JSONObject response) {
+                                try {
+                                    if (response.getString("id").equals("0")) {
+                                        starred = !starred;
+                                        ActionMenuItemView starmark = findViewById(R.id.actionStar);
+                                        MenuItem item = starmark.getItemData();
+                                        if (starred) {
+                                            item.setIcon(R.drawable.star);
+                                        } else {
+                                            item.setIcon(R.drawable.star_outline);
+                                        }
+                                    }
+                                    Toast.makeText(EntityDetailActivity.this,
+                                            response.getString("msg"),
+                                            Toast.LENGTH_SHORT).show();
+                                } catch (JSONException e) {
+                                    Log.e("Error parsing star resp obj", e.toString());
+                                }
+                            }
+                        }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+
+                    }
+                });
+                reqQue.add(req);
+
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
 }

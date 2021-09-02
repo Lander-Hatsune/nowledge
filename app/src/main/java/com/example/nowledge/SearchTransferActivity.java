8d238094ea6fd1312c09b162e98d8ec3710b67ef
Lander-Hatsune <wangwwx@yeah.net>
2021-09-02 08:52:41 +0800

    history (on memory)

--- app/src/main/java/com/example/nowledge/SearchTransferActivity.java
+++ app/src/main/java/com/example/nowledge/SearchTransferActivity.java
@@ -5,11 +5,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.util.Pair;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.Spinner;
+
+import com.example.nowledge.data.User;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -33,11 +38,36 @@ public class SearchTransferActivity extends AppCompatActivity {
         ArrayAdapter<String> adapter = new ArrayAdapter<String>
                 (this, R.layout.support_simple_spinner_dropdown_item, courseNames);
         spinner.setAdapter(adapter);
+
+        ListView hisListView = findViewById(R.id.historyListView);
+        List<String> historyStr = new ArrayList<>();
+        if (User.getHistory() != null) {
+            for (Pair<String, String> his : User.getHistory()) {
+                historyStr.add(his.second);
+            }
+            ArrayAdapter<String> hisAdaptor = new ArrayAdapter<>
+                    (this, R.layout.entity_short_item, historyStr);
+            hisListView.setAdapter(hisAdaptor);
+            hisListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                @Override
+                public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                    String course = User.getHistory().get(i).first;
+                    String name = User.getHistory().get(i).second;
+                    Log.d("selected course", course);
+                    Intent intentSearch = new Intent(SearchTransferActivity.this, SearchActivity.class);
+                    intentSearch.putExtra("key", name);
+                    intentSearch.putExtra("course", course);
+                    startActivity(intentSearch);
+                }
+            });
+        }
+
         searchButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 String key = searchkey.getText().toString();
                 String course = spinner.getSelectedItem().toString();
+                User.addHistory(course, key);
                 Log.d("selected course", course);
                 Intent intentSearch = new Intent(SearchTransferActivity.this, SearchActivity.class);
                 intentSearch.putExtra("key", key);
--- app/src/main/java/com/example/nowledge/data/User.java
+++ app/src/main/java/com/example/nowledge/data/User.java
@@ -1,10 +1,13 @@
 package com.example.nowledge.data;
 
+import android.util.Pair;
+
+import java.util.ArrayList;
 import java.util.List;
 
 public class User {
     private static String username = "0";
-    private static List<String> history;
+    private static List<Pair<String, String>> history = null;
     private static boolean loggedin = false;
 
     public static String getUsername() {
@@ -15,12 +18,15 @@ public class User {
         User.username = username;
     }
 
-    public static List<String> getHistory() {
+    public static List<Pair<String, String>> getHistory() {
         return history;
     }
 
-    public static void addHistory(String his) {
-        history.add(his);
+    public static void addHistory(String course, String name) {
+        if (history == null) {
+            history = new ArrayList<Pair<String, String>>();
+        }
+        history.add(new Pair<String, String>(course, name));
     }
 
     public static boolean isLoggedin() {

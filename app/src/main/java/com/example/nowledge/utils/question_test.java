2603fc3d63b3405dcd0dd3bb7d78cd57950fbbf9
xunzhen0016 <461431084@qq.com>
2021-09-09 14:56:32 +0800

    add question test

---
+++ app/src/main/java/com/example/nowledge/utils/question_test.java
@@ -0,0 +1,36 @@
+package com.example.nowledge.utils;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class question_test {
+
+    private String QuestionText;
+    private String Answer;
+    private String A;
+    private String B;
+    private String C;
+    private String D;
+
+    public question_test(String QuestionText,String Answer,String A,String B,String C,String D) throws JSONException {
+        this.QuestionText=QuestionText;
+        this.Answer=Answer;
+        this.A=A;
+        this.B=B;
+        this.C=C;
+        this.D=D;
+    }
+
+    public String getQuestionText(){return QuestionText;}
+
+    public String getAnswer(){return Answer;};
+
+    public String getA(){return A;}
+
+    public String getB(){return B;}
+
+    public String getC(){return C;}
+
+    public String getD(){return D;}
+
+}
